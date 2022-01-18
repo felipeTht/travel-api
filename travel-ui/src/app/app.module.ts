a83043aef95af142b89469df332ec542a035b467
Felipe Carvajal <felipe.tht@gmail.com>
2022-01-18 23:00:16 +0100

    add: angular material

--- travel-ui/src/app/app.module.ts
+++ travel-ui/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReservationComponent } from './feature/reservation/reservation.component';
+import { DashboardComponent } from './feature/dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/shared/module/material.module';
+import { HeaderComponent } from './core/header/header.component';
+import { SearchFlightComponent } from './feature/reservation/components/search-flight/search-flight.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ReservationComponent, DashboardComponent, HeaderComponent, SearchFlightComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
